b72fee90219ab3163934cf59d29f4a02eafea818
Valentine0604 <ewecia.s@gmail.com>
2024-05-18 14:02:45 +0200

    Implement field validation logic

--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -3,8 +3,12 @@ package com.ltp.gradesubmission;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.naming.Binding;
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,7 +35,9 @@ public class GradeController {
     }
 
     @PostMapping("/handleSubmit")
-    public String submitForm(Grade grade) {
+    public String submitForm(@Valid Grade grade, BindingResult result) {
+
+        if(result.hasErrors()) return "form";
 
         int index = getGradeIndex(grade.getId());
 
