a9c8def726424136e8655e97cf5eed6753a71217
Valentine0604 <ewecia.s@gmail.com>
2024-06-05 20:36:13 +0200

    Implement API

---
+++ src/main/java/com/ltp/gradesubmission/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.ltp.gradesubmission.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ltp.gradesubmission.entity.Student;
+
+/**
+ * StudentRepository
+ */
+public interface StudentRepository extends CrudRepository<Student, Long>{
+
+}
